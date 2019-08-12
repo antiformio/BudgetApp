749ffc2339a4249c2978f92e1f3859beab2da78e
antiformio <fjnmgm@gmail.com>
2019-08-12 16:52:40 +0200

    Commented serialize class

--- BudgetApp/serialize.py
+++ BudgetApp/serialize.py
@@ -7,6 +7,9 @@ from botocore.exceptions import NoCredentialsError
 
 class serialization():
     
+    """
+        Loads credentials from JSON and reads into class variables
+    """
     def readCredentials(self):
         with open('C:\\Users\\fhm\\source\\repos\\BudgetApp\\BudgetApp\\s3.json') as f:
             data = json.load(f)
@@ -18,12 +21,20 @@ class serialization():
     def __init__(self):
         self.ACCESS_KEY, self.SECRET_KEY, self.bucket = self.readCredentials()
         self.s3 = boto3.client('s3', aws_access_key_id=self.ACCESS_KEY , aws_secret_access_key=self.SECRET_KEY)
-
+    
+    """
+        Converts object (dataframe, dict, list, etc) to file and uploads to s3 bucket
+            After the upload, deletes the file created.
+    """
     def dfToFileUpload(self, df, fileName):
         df.to_pickle(fileName)
         self.AWSupload(fileName, fileName)
         os.remove(fileName)
-
+    
+    """
+        Converts a file downloaded to an object (dataframe, dict, list, etc)
+            After the download and convertion, deletes the local file
+    """
     def fileToDfDownload(self, fileName):
         self.AWSdownload(fileName, fileName)
         infile = open(fileName,'rb')
@@ -31,7 +42,10 @@ class serialization():
         infile.close()
         os.remove(fileName)
         return new_df
-
+    
+    """
+        Uploads specific file for s3 bucket
+    """
     def AWSupload(self, local_file, s3_file):
         try:
             self.s3.upload_file(local_file, self.bucket, s3_file)
@@ -42,7 +56,10 @@ class serialization():
         except NoCredentialsError:
             print("Credentials not available")
             return False
-
+    
+    """
+        Download a specific file from s3 bucket
+    """
     def AWSdownload(self, s3_fileName, localPath):
         try:
             #self.s3.download_file(self.bucket,s3_file, 'C:\\Users\\fhm\\Desktop\\dogs')
@@ -53,6 +70,10 @@ class serialization():
                     print("The object does not exist.")
             else:
                 raise
-
+    
+    """
+        Returns a json with all the files in bucket
+    """
     def getFilesOnBucket(self):
-        return self.s3.list_objects(Bucket = self.bucket, Prefix = 'df')
+        response = self.s3.list_objects(Bucket = self.bucket, Prefix = 'df')
+        return response['Contents']
